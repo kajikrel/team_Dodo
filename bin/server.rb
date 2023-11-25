658dea894f5913264b4c8fb1f0b411518b8bd818
HACHI3232 <harada.chisato526@gmail.com>
2023-11-25 16:31:41 +0900

    server.reの改修

--- bin/server.rb
+++ bin/server.rb
@@ -13,30 +13,32 @@ client = Mysql2::Client.new(
 )
 puts "Connected to the MySQL database successfully."
 
+# publicディレクトリのパスを取得
 root = File.expand_path '../public', __dir__
+
+# WEBrickサーバーの設定
 server = WEBrick::HTTPServer.new(
   Port: 8000,
-  DocumentRoot: root
+  DocumentRoot: root,
+  DirectoryIndex: ['index.erb'] # デフォルトのディレクトリインデックスファイルを指定
 )
-# CSS ファイルに対するリクエストの処理
-server.mount('../css', WEBrick::HTTPServlet::FileHandler, File.join(root, 'css'))
 
-# ERB ファイルの処理
-
-# CSS ファイルに対するリクエストの処理
+# CSS ファイルに対するリクエストを処理するためにFileHandlerをマウント
 server.mount('/css', WEBrick::HTTPServlet::FileHandler, File.join(root, 'css'))
 
-# JavaScript ファイルに対するリクエストの処理
+# JavaScript ファイルに対するリクエストを処理するためにFileHandlerをマウント
 server.mount('/js', WEBrick::HTTPServlet::FileHandler, File.join(root, 'js'))
 
 # ERB ファイルの処理
 server.mount_proc '/' do |req, res|
-  template_path = File.join(root, 'index.erb')
+  template_path = File.join(root, 'index.erb') # 正しいERBファイルのパスを指定
   erb = ERB.new(File.read(template_path))
-  res.content_type = 'text/html; charset=UTF-8'
-  res.body = erb.result(binding)
+  res.content_type = 'text/html; charset=UTF-8' # コンテントタイプを指定
+  res.body = erb.result(binding) # ERBテンプレートをレンダリングしてレスポンスのボディに設定
 end
 
+# Ctrl+Cが押されたときにサーバーをシャットダウンするためのトラップを設定
 trap 'INT' do server.shutdown end
 
+# サーバーを起動
 server.start
